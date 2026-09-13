5b2de4f537e93bfc4c9aa82f5d1dae24b6706466
kuratiep70-engfgft4r4 <kuratiep70@gmail.com>
2026-09-13 15:07:47 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+name = "Kur Atiep Kur"
+age = 25
+favourite_language = "python"
+goal = "To become a professional web developer"
+print(f"My name is {name} .  I'm {age} years old. my favourite language is {favourite_language} my main goal is {goal}")
+
+kuratiep70@gmail.com
