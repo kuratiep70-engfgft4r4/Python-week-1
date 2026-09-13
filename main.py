dd11c132c05611ba518ad9f4a49057829e7e4fc4
kuratiep70-engfgft4r4 <kuratiep70@gmail.com>
2026-09-13 15:31:41 +0300

    Update personal details output

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+# Store your details in variables
+
 name = "Kur Atiep Kur"
 age = 25
 favourite_language = "python"
 goal = "To become a professional web developer"
-print(f"My name is {name} .  I'm {age} years old. my favourite language is {favourite_language} my main goal is {goal}")
-
-kuratiep70@gmail.com
+# Display them
+print(f"My name is {name} .  I'm {age} years old.")
+print(f"My favourite language is {favourite_language} my main goal is {goal}")
